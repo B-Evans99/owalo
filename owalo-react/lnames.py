5c3b8c869daddc02bccc7da3d01d05fa99da0b40
BEvans99 <bevans@bclc.com>
2019-11-28 10:13:36 -0800

    feat(lnames.py): print out json files

--- owalo-react/lnames.py
+++ owalo-react/lnames.py
@@ -1,5 +1,29 @@
+import json
+
 with open("irish.txt") as source:
-    read = source.read().split()
+    read = source.read().upper().split()
+
+totals = {}
 
 for line in read:
-    print(line)
+    for i in range(len(line)-2):
+        if(line[i] not in totals):
+            totals[line[i]] = {}
+        if(line[i+1] not in totals[line[i]]):
+            totals[line[i]][line[i+1]] = 0
+        totals[line[i]][line[i+1]] += 1
+    if(line[len(line)-1] not in totals):
+        totals[line[len(line)-1]] = {}
+    if(" " not in totals[line[len(line)-1]]):
+        totals[line[len(line)-1]][" "] = 0
+    totals[line[len(line)-1]][" "] += 1
+
+for k in totals:
+    count = 0
+    for sk in totals[k]:
+        count += totals[k][sk]
+    for sk in totals[k]:
+        totals[k][sk] = totals[k][sk]/count
+
+# print(totals)
+print(json.dumps(totals))
