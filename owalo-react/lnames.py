69a299feecf840723e14abf58d33d80dc8769e46
BEvans99 <bevans@bclc.com>
2019-11-28 10:15:54 -0800

    feat(lnames): take in file name as command line argument

--- owalo-react/lnames.py
+++ owalo-react/lnames.py
@@ -1,29 +1,35 @@
 import json
+import sys
 
-with open("irish.txt") as source:
-    read = source.read().upper().split()
+try:
+    fname = sys.argv[1]
 
-totals = {}
+    with open(fname) as source:
+        read = source.read().upper().split()
 
-for line in read:
-    for i in range(len(line)-2):
-        if(line[i] not in totals):
-            totals[line[i]] = {}
-        if(line[i+1] not in totals[line[i]]):
-            totals[line[i]][line[i+1]] = 0
-        totals[line[i]][line[i+1]] += 1
-    if(line[len(line)-1] not in totals):
-        totals[line[len(line)-1]] = {}
-    if(" " not in totals[line[len(line)-1]]):
-        totals[line[len(line)-1]][" "] = 0
-    totals[line[len(line)-1]][" "] += 1
+    totals = {}
 
-for k in totals:
-    count = 0
-    for sk in totals[k]:
-        count += totals[k][sk]
-    for sk in totals[k]:
-        totals[k][sk] = totals[k][sk]/count
+    for line in read:
+        for i in range(len(line)-2):
+            if(line[i] not in totals):
+                totals[line[i]] = {}
+            if(line[i+1] not in totals[line[i]]):
+                totals[line[i]][line[i+1]] = 0
+            totals[line[i]][line[i+1]] += 1
+        if(line[len(line)-1] not in totals):
+            totals[line[len(line)-1]] = {}
+        if(" " not in totals[line[len(line)-1]]):
+            totals[line[len(line)-1]][" "] = 0
+        totals[line[len(line)-1]][" "] += 1
 
-# print(totals)
-print(json.dumps(totals))
+    for k in totals:
+        count = 0
+        for sk in totals[k]:
+            count += totals[k][sk]
+        for sk in totals[k]:
+            totals[k][sk] = totals[k][sk]/count
+
+    # print(totals)
+    print(json.dumps(totals))
+except:
+    print("Please provide a file as an argument.")
